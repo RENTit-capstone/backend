5bc40905ee3b2d039b695677a68903845cb896de
Junwoo <pjw7200@ajou.ac.kr>
2025-04-12 01:09:49 +0900

    Feat: JWT 발급 Provider 추가

---
+++ src/main/java/com/capstone/rentit/login/provider/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package com.capstone.rentit.login.provider;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+
+    private final Key key;
+    private final long jwtExpirationInMs;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long expiration) {
+//        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.jwtExpirationInMs = expiration;
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        // signWith 메서드는 이제 Key 객체와 SignatureAlgorithm을 함께 넘겨줍니다.
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(authToken);
+            return true;
+        } catch (SecurityException | MalformedJwtException ex) {
+            log.error("Invalid JWT signature or token", ex);
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token", ex);
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token", ex);
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.", ex);
+        }
+        return false;
+    }
+}
