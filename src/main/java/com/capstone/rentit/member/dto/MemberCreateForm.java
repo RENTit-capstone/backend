81bd8c1da459dbd99eb1e7188a5fd60027dc56ff
Junwoo <pjw7200@ajou.ac.kr>
2025-04-14 02:18:05 +0900

    Refactor: role 삭제, @JsonSubType 추가

--- src/main/java/com/capstone/rentit/member/dto/MemberCreateForm.java
+++ src/main/java/com/capstone/rentit/member/dto/MemberCreateForm.java
@@ -14,13 +14,13 @@ import lombok.Setter;
         property = "memberType" // 클라이언트가 "memberType" 속성으로 타입을 지정 (예: "STUDENT")
 )
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = StudentCreateForm.class, name = "STUDENT")
-        // 추후 다른 타입(예: COMPANY, STUDENT_COUNCIL)이 있으면 여기에 추가
+        @JsonSubTypes.Type(value = StudentCreateForm.class, name = "STUDENT"),
+        @JsonSubTypes.Type(value = StudentCouncilMemberCreateForm.class, name = "COUNCIL"),
+        @JsonSubTypes.Type(value = CompanyCreateForm.class, name = "COMPANY")
 })
 public abstract class MemberCreateForm {
     private String email;
     private String password;
     private String name;
-    private Integer role;
     private String profileImg;
 }
