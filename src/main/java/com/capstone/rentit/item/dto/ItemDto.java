3e5ba5c15cf3f30bee7358750c7ad07f1573e96a
Junwoo <pjw7200@ajou.ac.kr>
2025-04-28 22:56:18 +0900

    Refactor: entity to dto 변환 dto method로 통합

--- src/main/java/com/capstone/rentit/item/dto/ItemDto.java
+++ src/main/java/com/capstone/rentit/item/dto/ItemDto.java
@@ -1,5 +1,6 @@
 package com.capstone.rentit.item.dto;
 
+import com.capstone.rentit.item.domain.Item;
 import com.capstone.rentit.item.status.ItemStatusEnum;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,4 +26,23 @@ public class ItemDto {
     private LocalDateTime endDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public static ItemDto fromEntity(Item item) {
+        return ItemDto.builder()
+                .itemId(item.getItemId())
+                .ownerId(item.getOwnerId())
+                .name(item.getName())
+                .itemImg(item.getItemImg())
+                .description(item.getDescription())
+                .categoryId(item.getCategoryId())
+                .price(item.getPrice())
+                .status(item.getStatus())
+                .damagedPolicy(item.getDamagedPolicy())
+                .returnPolicy(item.getReturnPolicy())
+                .startDate(item.getStartDate())
+                .endDate(item.getEndDate())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
 }
