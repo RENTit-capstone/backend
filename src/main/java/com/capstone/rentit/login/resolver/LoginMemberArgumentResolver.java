c362cd702fab789e4fe5cede5332f6ee2e7fe4fc
Junwoo <pjw7200@ajou.ac.kr>
2025-05-02 22:53:18 +0900

    Refactor: MemberDtoFactory 삭제

--- src/main/java/com/capstone/rentit/login/resolver/LoginMemberArgumentResolver.java
+++ src/main/java/com/capstone/rentit/login/resolver/LoginMemberArgumentResolver.java
@@ -1,18 +1,23 @@
 package com.capstone.rentit.login.resolver;
 
+import com.capstone.rentit.file.service.FileStorageService;
 import com.capstone.rentit.login.annotation.Login;
 import com.capstone.rentit.login.dto.MemberDetails;
 import com.capstone.rentit.member.dto.MemberDto;
-import com.capstone.rentit.member.dto.MemberDtoFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final FileStorageService fileStorageService;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -29,6 +34,6 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
         if (auth == null || !(auth.getPrincipal() instanceof MemberDetails details)) {
             return null; // 인증되지 않은 경우, 필요시 예외 처리 가능
         }
-        return MemberDtoFactory.toDto(details.getMember());
+        return MemberDto.fromEntity(details.getMember(), fileStorageService.generatePresignedUrl(details.getMember().getProfileImg()));
     }
 }
