3acd28bf8074e957f9e2ca0d53bd62af52f06907
Junwoo <pjw7200@ajou.ac.kr>
2025-05-07 01:17:37 +0900

    Feat: 사물함 생성, 조회 폼 추가

---
+++ src/main/java/com/capstone/rentit/locker/dto/LockerCreateForm.java
@@ -0,0 +1,13 @@
+package com.capstone.rentit.locker.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class LockerCreateForm {
+    String university;
+    String locationDescription;
+}
