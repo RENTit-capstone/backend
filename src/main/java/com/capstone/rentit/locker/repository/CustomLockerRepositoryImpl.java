e36d56686a74a3fd24de7ec2fb1bdf3a9d9c72e0
Junwoo <pjw7200@ajou.ac.kr>
2025-05-07 23:48:25 +0900

    Refactor: queryFactory 변수명 수정

--- src/main/java/com/capstone/rentit/locker/repository/CustomLockerRepositoryImpl.java
+++ src/main/java/com/capstone/rentit/locker/repository/CustomLockerRepositoryImpl.java
@@ -1,24 +1,20 @@
 package com.capstone.rentit.locker.repository;
 
 import com.capstone.rentit.locker.domain.Locker;
+import com.capstone.rentit.locker.domain.QLocker;
 import com.capstone.rentit.locker.dto.LockerSearchForm;
-import com.capstone.rentit.locker.event.RentalLockerAction;
-import com.capstone.rentit.rental.domain.QRental;
-import com.capstone.rentit.rental.domain.Rental;
-import com.capstone.rentit.rental.status.RentalStatusEnum;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class CustomLockerRepositoryImpl implements CustomLockerRepository {
 
-    private final JPAQueryFactory query;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public List<Locker> search(LockerSearchForm form) {
@@ -32,7 +28,7 @@ public class CustomLockerRepositoryImpl implements CustomLockerRepository {
             builder.and(locker.available.eq(form.getAvailable()));
         }
 
-        return query
+        return queryFactory
                 .selectFrom(locker)
                 .where(builder)
                 .orderBy(locker.lockerId.asc())
