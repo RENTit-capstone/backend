d7fabfefdb8ded66b0a18706ae6d6e61aa529020
Junwoo <pjw7200@ajou.ac.kr>
2025-05-13 23:52:21 +0900

    Test: 잔액 확인 메서드 추가

--- src/main/java/com/capstone/rentit/payment/domain/Wallet.java
+++ src/main/java/com/capstone/rentit/payment/domain/Wallet.java
@@ -22,8 +22,12 @@ public class Wallet {
     }
 
     public void withdraw(long amount) {
+        checkBalance(amount);
+        balance -= amount;
+    }
+
+    public void checkBalance(long amount) {
         if (amount <= 0) throw new IllegalArgumentException("amount > 0");
         if (balance < amount) throw new InsufficientBalanceException("잔액이 부족합니다. 현재 잔액: " + balance + " 결제 금액" + amount);
-        balance -= amount;
     }
 }
