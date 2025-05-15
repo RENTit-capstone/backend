09fb0858371cd7237925b3d1606fd7a1770cd26c
Junwoo <pjw7200@ajou.ac.kr>
2025-05-15 20:43:07 +0900

    Refactor: RestTemplate 생성 수정

--- src/main/java/com/capstone/rentit/config/ApiConfig.java
+++ src/main/java/com/capstone/rentit/config/ApiConfig.java
@@ -1,14 +1,24 @@
 package com.capstone.rentit.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class ApiConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        factory.setReadTimeout(Duration.ofSeconds(10));
+
+        return builder
+                .requestFactory(() -> factory)
+                .build();
     }
 }
