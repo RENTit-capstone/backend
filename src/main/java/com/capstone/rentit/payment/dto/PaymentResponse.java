58ba2a1fc54f57aacd9c7ec6a00558a773abdde4
Junwoo <pjw7200@ajou.ac.kr>
2025-05-16 23:44:16 +0900

    Feat: 거래 정보 추가

--- src/main/java/com/capstone/rentit/payment/dto/PaymentResponse.java
+++ src/main/java/com/capstone/rentit/payment/dto/PaymentResponse.java
@@ -1,6 +1,31 @@
 package com.capstone.rentit.payment.dto;
 
+import com.capstone.rentit.payment.domain.Payment;
 import com.capstone.rentit.payment.type.PaymentStatus;
+import com.capstone.rentit.payment.type.PaymentType;
+import com.capstone.rentit.rental.domain.Rental;
+import lombok.Builder;
 
-public record PaymentResponse(Long paymentId, PaymentStatus status) {}
+import java.time.LocalDateTime;
+
+@Builder
+public record PaymentResponse(
+        Long paymentId,
+        PaymentType type,
+        PaymentStatus status,
+        long amount,
+        LocalDateTime createdAt,
+        Rental rental
+) {
+    public static PaymentResponse fromEntity(Payment entity){
+        return PaymentResponse.builder()
+                .paymentId(entity.getId())
+                .type(entity.getType())
+                .status(entity.getStatus())
+                .amount(entity.getAmount())
+                .createdAt(entity.getCreatedAt())
+                .rental(entity.getRental() == null ? null : entity.getRental())
+                .build();
+    }
+}
 
