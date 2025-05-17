4240dacad488a7eeba2b0f673ba38e18d1ace72c
Junwoo <pjw7200@ajou.ac.kr>
2025-05-17 22:08:58 +0900

    Feat: 알림 예외, 핸들러 추가

---
+++ src/main/java/com/capstone/rentit/notification/exception/NotificationNotFound.java
@@ -0,0 +1,7 @@
+package com.capstone.rentit.notification.exception;
+
+public class NotificationNotFound extends RuntimeException {
+    public NotificationNotFound(String message) {
+        super(message);
+    }
+}
