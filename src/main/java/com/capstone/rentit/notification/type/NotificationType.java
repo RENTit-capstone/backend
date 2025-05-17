23001c01e0753f1e9e8ced325d9f1565cfff62a2
Junwoo <pjw7200@ajou.ac.kr>
2025-05-17 22:09:23 +0900

    Feat: 알림 타임 추가

---
+++ src/main/java/com/capstone/rentit/notification/type/NotificationType.java
@@ -0,0 +1,16 @@
+package com.capstone.rentit.notification.type;
+
+public enum NotificationType {
+
+    /* ─── 물품 소유자 ─── */
+    RENT_REQUESTED,          // 대여자가 대여 요청
+    ITEM_RETURNED,           // 대여자가 반납 완료
+    RENT_START_D_3,          // 대여 시작 3일 전
+    RENT_START_D_0,          // 대여 시작 당일
+
+    /* ─── 물품 대여자 ─── */
+    REQUEST_ACCEPTED,        // 소유자가 신청 승낙
+    ITEM_PLACED,             // 소유자가 사물함에 물건 넣음
+    RENT_END_D_3,            // 대여 만료 3일 전
+    RENT_END_D_0             // 대여 만료 당일
+}
