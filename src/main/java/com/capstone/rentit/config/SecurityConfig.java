f154552d2dc3cafa123731f051b8960b59f90de4
Junwoo <pjw7200@ajou.ac.kr>
2025-05-26 20:03:30 +0900

    Chore: 권한 해제

--- src/main/java/com/capstone/rentit/config/SecurityConfig.java
+++ src/main/java/com/capstone/rentit/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll() // 인증 없이 접근 허용
-                        .requestMatchers("/api/v1/admin/**", "/actuator/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
