82c0855f6beaa42c91789d6591911d886e890d00
Junwoo <pjw7200@ajou.ac.kr>
2025-05-29 00:01:43 +0900

    Chore: memberId 삭제

--- src/main/java/com/capstone/rentit/inquiry/dto/InquiryCreateForm.java
+++ src/main/java/com/capstone/rentit/inquiry/dto/InquiryCreateForm.java
@@ -3,7 +3,6 @@ package com.capstone.rentit.inquiry.dto;
 import com.capstone.rentit.inquiry.type.InquiryType;
 
 public record InquiryCreateForm(
-        Long memberId,
         String title,
         String content,
         InquiryType type
