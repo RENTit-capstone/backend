f0a25c3a8d5850c383bde2f4281204afec421c5e
Junwoo <pjw7200@ajou.ac.kr>
2025-05-29 00:02:09 +0900

    Chore: 파손 타입 추가

--- src/main/java/com/capstone/rentit/inquiry/type/InquiryType.java
+++ src/main/java/com/capstone/rentit/inquiry/type/InquiryType.java
@@ -2,5 +2,6 @@ package com.capstone.rentit.inquiry.type;
 
 public enum InquiryType {
     SERVICE,   // 서비스 이용 문의
-    REPORT     // 신고/제보 문의
+    REPORT,     // 신고/제보 문의
+    DAMAGE     // 파손 신고
 }
