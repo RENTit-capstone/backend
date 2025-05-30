87ce36b98905725c60ecc27684fd85b8de14ec6e
Junwoo <pjw7200@ajou.ac.kr>
2025-05-30 23:02:38 +0900

    Chore: rentalId 기반 조회

--- src/main/java/com/capstone/rentit/payment/repository/PaymentRepository.java
+++ src/main/java/com/capstone/rentit/payment/repository/PaymentRepository.java
@@ -4,5 +4,9 @@ import com.capstone.rentit.payment.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long>, CustomPaymentRepository { }
+public interface PaymentRepository extends JpaRepository<Payment, Long>, CustomPaymentRepository {
+    Optional<Payment> findByRentalId(Long rentalId);
+}
