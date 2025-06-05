122f42b11800025c33900c132e22ad1f12f2feda
Junwoo <pjw7200@ajou.ac.kr>
2025-06-05 18:14:45 +0900

    Feat: 연체 대여 조회

--- src/main/java/com/capstone/rentit/rental/repository/RentalRepository.java
+++ src/main/java/com/capstone/rentit/rental/repository/RentalRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,4 +23,6 @@ public interface RentalRepository extends JpaRepository<Rental, Long>, CustomRen
     List<Rental> findByDueDate(LocalDate endDate);
 
     Optional<Rental> findTopByItemIdAndStatus(Long itemId, RentalStatusEnum status);
+
+    List<Rental> findByStatusInAndDueDateBefore(List<RentalStatusEnum> statuses, LocalDateTime dateTime);
 }
